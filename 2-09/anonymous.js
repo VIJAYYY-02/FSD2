ff2e9160c573e0b909ab2ad0d74c3d55f7dd6e69
vijay <vijay.25b01010780@abes.ac.in>
2026-09-02 09:08:37 +0530

    .

--- 2-09/anonymous.js
+++ 2-09/anonymous.js
@@ -2,13 +2,18 @@
 // (function() {
 //     console.log("This is an anonymous function");
 // })();
-console.log(one(2));
-function one(n) {
-    return (n+1);
-}
+// console.log(one(2));
+// function one(n) {
+//     return (n+1);
+// }
 
-console.log(two(5));
-const two = function(n) {
-    return (n+2);
-}
+// console.log(two(5));
+// const two = function(n) {
+//     return (n+2);
+// }
+
+setTimeout(function() {
+    console.log("This is an anonymous function inside setTimeout");
+});
+
 

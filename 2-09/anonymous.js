8619e0324eabaab924a0a4a21e27c538ebab504d
vijay <vijay.25b01010780@abes.ac.in>
2026-09-02 09:07:20 +0530

    .

---
+++ 2-09/anonymous.js
@@ -0,0 +1,14 @@
+// //anpnymous function
+// (function() {
+//     console.log("This is an anonymous function");
+// })();
+console.log(one(2));
+function one(n) {
+    return (n+1);
+}
+
+console.log(two(5));
+const two = function(n) {
+    return (n+2);
+}
+

46734c503949ebdaa9bd3d0e87eac3b18e1fbb3e
vijay <vijay.25b01010780@abes.ac.in>
2026-09-02 09:27:30 +0530

    this function

--- 2-09/this.js
+++ 2-09/this.js
@@ -2,7 +2,12 @@ const person = {
   name: "vj",
   age: 25,
   greet: function() {
-    console.log(`Hello, my name is ${this.name} and I am ${this.age} years old.`);
+    // console.log(`Hello, my name is ${this.name} and I am ${this.age} years old.`);
+    console.log(this);
   }
 };
+person.greet();
+
+person.name = "vj1";
+person.age = 26;
 person.greet();

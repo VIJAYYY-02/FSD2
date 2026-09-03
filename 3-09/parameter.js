972b26324022857d8d86861477df47b08fe4642d
vijay <vijay.25b01010780@abes.ac.in>
2026-09-03 10:08:35 +0530

    try nd catch

--- 3-09/parameter.js
+++ 3-09/parameter.js
@@ -1,9 +1,24 @@
-function display(a, b) {
-    if (a === undefined || b === undefined) {
-        return "Please provide both values";
-    }
-    return ` ${a + b}`;
+// function display(a, b) {
+//     if (a === undefined || b === undefined) {
+//         return "Please provide both values";
+//     }
+//     return `${a + b}`;
+// }
+// console.log(display(5, 10));
+// console.log(display());
+
+
+
+function display(a=requiredarg(), b=requiredarg()) {
+    return `${a + b}`;
 }
-console.log(display(5, 10));
-console.log(display());
+function requiredarg() {
+    throw new Error("Please provide both values");
+}
+try {
+    console.log(display(5, 10));
+    console.log(display());
+}catch (error) {
+    console.error(error.message);
+}
 

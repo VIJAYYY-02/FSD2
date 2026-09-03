559ed614625eee295cc16820714fc42b8bc0597c
vijay <vijay.25b01010780@abes.ac.in>
2026-09-03 10:15:21 +0530

    /

--- 3-09/parameter.js
+++ 3-09/parameter.js
@@ -9,16 +9,21 @@
 
 
 
-function display(a=requiredarg(), b=requiredarg()) {
-    return `${a + b}`;
-}
-function requiredarg() {
-    throw new Error("Please provide both values");
-}
-try {
-    console.log(display(5, 10));
-    console.log(display());
-}catch (error) {
-    console.error(error.message);
-}
+// function display(a=requiredarg(), b=requiredarg()) {
+//     return `${a + b}`;
+// }
+// function requiredarg() {
+//     throw new Error("Please provide both values");
+// }
+// try {
+//     console.log(display(5, 10));
+//     console.log(display());
+// }catch (error) {
+//     console.error(error.message);
+// }
 
+function show(x=1,y=x,z=y+x) {
+    console.log(x+y+z);
+}
+console.log(show());
+console.log(show(5));

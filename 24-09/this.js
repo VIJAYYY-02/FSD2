d53eaa94c72916839814fbed5cbd585b29bf5723
vijay <vijay.25b01010780@abes.ac.in>
2026-09-24 09:20:28 +0530

    /

--- 24-09/this.js
+++ 24-09/this.js
@@ -1,7 +1,13 @@
-const person={
-    name:"vj",
-    age:"19",
-    greet:function (){
-        console.log(this);}
-};
-person.greet();
+// const person={
+//     name:"vj",
+//     age:"19",
+//     greet:function (){
+//         console.log(this);}
+// };
+// person.greet();
+
+// function one(){
+//     let user="vj";
+//     console.log(this.user);
+// }
+// one();

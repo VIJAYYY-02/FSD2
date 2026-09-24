509300361a9fd8e0182c9992063ab963183f9df4
vijay <vijay.25b01010780@abes.ac.in>
2026-09-24 10:20:38 +0530

    /

--- 24-09/this.js
+++ 24-09/this.js
@@ -31,10 +31,34 @@
 // };
 // let p1 = person.greet.bind(person);
 // console.log(p1());
-function multi(a, b) {
-    console.log("a =", a, "b =", b);
-    return a * b;
+// function multi(a, b) {
+//     console.log("a =", a, "b =", b);
+//     return a * b;
+// }
+
+// const d = multi.bind(null, 2);
+// console.log(d(5)); // logs a=2, b=5 → result 10
+
+
+// function person(name){
+//     this.name=name;
+
+// }
+// person.prototype.getname=function(){
+//     return this.name;
+// }
+// let p1= new person("vj");
+// console.log(p1.name);
+// console.log(p1.getname);
+function person(name){
+    this.name = name;
 }
 
-const d = multi.bind(null, 2);
-console.log(d(5)); // logs a=2, b=5 → result 10
+// attach to prototype
+person.prototype.getname = function(){
+    return this.name;
+}
+
+let p1 = new person("vj");
+console.log(p1.name);       // "vj"
+console.log(p1.getname());  // "vj"

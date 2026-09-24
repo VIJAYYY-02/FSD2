49389c55c27d0924f11d660e94b6d768719e07e3
vijay <vijay.25b01010780@abes.ac.in>
2026-09-24 09:41:11 +0530

    /

--- 24-09/this.js
+++ 24-09/this.js
@@ -11,3 +11,22 @@
 //     console.log(this.user);
 // }
 // one();
+
+// console.log(this===window);
+
+// function show(){
+//     console.log(this === global);
+// };
+// show();
+// global.show = show;
+// global.show();
+
+const person={
+    name:"vj",
+    age:"19",
+    greet:function(){
+        return this.name;
+    }
+};
+let p1= person.greet;
+console.log(p1());
